812b8ca62aee01a5a11f68f100a05f05a5c9fa5d
nonameprogram <webdemon.bartek@gmail.com>
2026-01-14 16:35:12 +0100

    fix: disable error from mobile dev tools

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,14 +42,14 @@ export default function Home() {
                 patternScale={3}
                 patternDensity={1.2}
                 pixelSizeJitter={0.5}
-                enableRipples
-                rippleSpeed={0.4}
-                rippleThickness={0.12}
-                rippleIntensityScale={1.5}
-                liquid
-                liquidStrength={0.12}
-                liquidRadius={1.2}
-                liquidWobbleSpeed={5}
+                enableRipples={false}
+                // rippleSpeed={0.4}
+                // rippleThickness={0.12}
+                // rippleIntensityScale={1.5}
+                liquid={false}
+                // liquidStrength={0.12}
+                // liquidRadius={1.2}
+                // liquidWobbleSpeed={5}
                 speed={0.6}
                 edgeFade={0.25}
                 transparent
