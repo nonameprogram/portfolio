5ceedd46bd93b0b335e6e6ad0ea307eb29084bc4
nonameprogram <webdemon.bartek@gmail.com>
2026-01-14 09:27:47 +0100

    fix: restore card original content

--- src/app/-components/sections/about-me-section.tsx
+++ src/app/-components/sections/about-me-section.tsx
@@ -148,22 +148,22 @@ export const AboutMeSection = () => {
               {
                 icon: <SiTypescript className="fill-violet-300 size-6" />,
                 title: "Type-safe approach",
-                text: "Seamlessly bringing API types into the web environment with OpenAPI, monorepos, and automated code generation — ensuring reliability and reducing runtime errors.",
+                text: "Seamlessly bringing API types into the web environment with OpenAPI, monorepos, and automated code generation — ensuring reliability and reducing runtime errors, regardless of the programming language or framework.",
               },
               {
                 icon: <RocketIcon className="text-violet-300 size-6" />,
                 title: "Performance",
-                text: "Optimizing build and test times by picking right tools, caching and making sure code-splitting is effective.",
+                text: "Optimizing build and test times by picking right tools, caching and making sure code-splitting is effective. Continuously tracking and eliminating latency and performance bottlenecks in applications.",
               },
               {
                 icon: <FlaskConicalIcon className="text-violet-300 size-6" />,
                 title: "Testing & Quality",
-                text: "Ensuring high code quality through unit, integration, and end-to-end testing.",
+                text: "Ensuring high code quality through unit, integration, and end-to-end testing. Catching issues early and maintaining confidence when refactoring or shipping new features — and sleeping well at night.",
               },
               {
                 icon: <ShieldIcon className="text-violet-300 size-6" />,
                 title: "Security",
-                text: "Applying modern security standards and best practices without compromising performance or speed.",
+                text: "Applying modern security standards and best practices, including secure authentication flows, proper data validation, monitoring including OpenTelemetry and safe API communication — without compromising performance or speed.",
               },
             ].map((item, i) => (
               <motion.div
